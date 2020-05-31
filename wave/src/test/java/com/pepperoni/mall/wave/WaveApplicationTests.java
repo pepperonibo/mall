73428aa740f2cd8256899fb756d4a4e62d5382e4
pepperoni <1074634342@qq.com>
2020-05-31 10:03:02 +0800

    init backend

---
+++ wave/src/test/java/com/pepperoni/mall/wave/WaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pepperoni.mall.wave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
